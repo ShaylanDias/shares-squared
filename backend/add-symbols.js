c8800cb31cdf3b62911f3539a3deff76372c389d
Shaylan Dias <shaylandias@berkeley.edu>
2021-01-09 19:23:53 -0800

    delete endpoint works

--- backend/add-symbols.js
+++ backend/add-symbols.js
@@ -9,30 +9,30 @@ const tableName = "watchlists";
  * Returns the symbol if it is invalid, else true
  * @param {*} symbol
  */
-// const checkValidity = async (symbol) => {
-//   let req = unirest("GET", "https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete");
+const checkValidity = async (symbol) => {
+  let req = unirest("GET", "https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete");
 
-//   req.query({
-//     "q": symbol,
-//     "region": "US"
-//   });
+  req.query({
+    "q": symbol,
+    "region": "US"
+  });
 
-//   req.headers({
-//     "x-rapidapi-key": process.env.RAPIDAPI_KEY,
-//     "x-rapidapi-host": process.env.RAPIDAPI_HOST,
-//     "useQueryString": true
-//   });
+  req.headers({
+    "x-rapidapi-key": process.env.RAPIDAPI_KEY,
+    "x-rapidapi-host": process.env.RAPIDAPI_HOST,
+    "useQueryString": true
+  });
 
-//   const res = await req.send();
-//   if (res.error) return symbol;
-//   let quotes = res.body.quotes;
-//   for (let quote of quotes) {
-//     if (quote.symbol === symbol) {
-//       return true;
-//     }
-//   }
-//   return false;
-// };
+  const res = await req.send();
+  if (res.error) return symbol;
+  let quotes = res.body.quotes;
+  for (let quote of quotes) {
+    if (quote.symbol === symbol) {
+      return true;
+    }
+  }
+  return false;
+};
 
 
 // const input = {
@@ -46,29 +46,30 @@ export const main = handler(async (event, context) => {
 
   // check user ID permissions
 
+  // throw error if watchlist does not exist
+
   // Check stock symbol validity
   const symbols = data.symbols;
-  // // Resolve validity on all symbols asynchronously
-  // const validities = await Promise.all(symbols.map(ticker => checkValidity(ticker)));
-  // // Filter validities to only the invalidSymbols left
-  // const invalid = validities.filter(validity => validity !== true);
-  // // Return if invalid.
-  // if (invalid.length > 0) {
-  //   throw new Error(`Invalid tickers: ${invalid}`);
-  // }
-  // add ticker to database
+  // Resolve validity on all symbols asynchronously
+  const validities = await Promise.all(symbols.map(ticker => checkValidity(ticker)));
+  // Filter validities to only the invalidSymbols left
+  const invalid = validities.filter(validity => validity !== true);
+  // Return if invalid.
+  if (invalid.length > 0) {
+    throw new Error(`Invalid tickers: ${invalid}`);
+  }
 
-  // const params = {
-  //   TableName: tableName,
-  //   Key: {
-  //     userId: "123",
-  //     watchlistId: data.watchlist,
-  //   },
-  //   UpdateExpression: "SET symbols = :symbols",
-  //   ExpressionAttributeValues: {
-  //     ":symbols": dynamoDb.createSet(symbols)
-  //   },
-  // };
+  const params = {
+    TableName: tableName,
+    Key: {
+      userId: "123",
+      watchlistId: data.watchlist,
+    },
+    UpdateExpression: "SET symbols = :symbols",
+    ExpressionAttributeValues: {
+      ":symbols": dynamoDb.createSet(symbols)
+    },
+  };
 
   const params = {
     TableName: tableName,
